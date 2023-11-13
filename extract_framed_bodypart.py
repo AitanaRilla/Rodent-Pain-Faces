8d0054af0dd4d21271641ed984720d171aa2d909
AitanaRilla <68219639+AitanaRilla@users.noreply.github.com>
2023-11-13 13:26:40 +0100

    Update extract_framed_bodypart.py

--- extract_framed_bodypart.py
+++ extract_framed_bodypart.py
@@ -3,10 +3,10 @@ import os
 import cv2
 from tqdm import tqdm
 
-# Ruta del archivo de video
+#Path of videos, change if moving
 video_path = '/Users/aitanarilla/Desktop/Modelo_caras/Files/E3_camara1_2000-01-01_00.04.02.mp4'
 
-# Abrir h5 y obtener coor x e y
+# Open .h5 file and get X and Y coordinates
 h5_file = '/Users/aitanarilla/Desktop/Modelo_caras/Files/E3_camara1_2000-01-01_00.04.02DLC_resnet50_CIA_ratsNov19shuffle1_1030000.h5'
 line_number = 381
 
