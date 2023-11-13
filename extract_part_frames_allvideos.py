3bfc7052b251ccec3c398ae6909319795f487650
AitanaRilla <68219639+AitanaRilla@users.noreply.github.com>
2023-11-13 13:33:11 +0100

    Update extract_part_frames_allvideos.py

--- extract_part_frames_allvideos.py
+++ extract_part_frames_allvideos.py
@@ -24,25 +24,25 @@ def procesar_carpeta(carpeta_path):
     if not os.path.isdir(carpeta_path):  # Check if it's a directory
         return
 
-    # Get list of files in the folder
+    #Get list of files in the folder
     archivos = os.listdir(carpeta_path)
 
     for nombre_fichero_mp4 in [f for f in archivos if f.endswith(".mp4")]:
         nombre_fichero = nombre_fichero_mp4[:-4]  # Removes the .mp4 extension
         video_path = os.path.join(carpeta_path, nombre_fichero_mp4)
 
-        # Search for corresponding .h5 file
+        #Search for the corresponding .h5 file
         h5_files = [f for f in archivos if f.startswith(nombre_fichero) and f.endswith('.h5')]
         if not h5_files:
             print(f"No se encontró ningún archivo h5 correspondiente para {nombre_fichero}.")
             continue
         h5_file = f"{carpeta_path}/{h5_files[0]}"
 
-        # Path to save the frames
+        #Path to save the frames
         directorio_destino = os.path.join(carpeta_path, nombre_fichero)
         verificar_crear_directorio(directorio_destino)
 
-        # Read the .h5 file and load it into a Pandas DataFrame
+        #Read the .h5 file and load it into a Pandas DataFrame
         try:
             df = pd.read_hdf(h5_file)
         except FileNotFoundError:
@@ -52,7 +52,7 @@ def procesar_carpeta(carpeta_path):
             print(f"An error occurred while trying to read the .h5 file: {str(e)}")
             return
 
-        # Obtain all coordinates for each frame
+        #Obtain all coordinates for each frame
         frame_coordinates = {}
         for frame_number in tqdm(range(len(df))):  
             coordinates = {}
@@ -64,21 +64,21 @@ def procesar_carpeta(carpeta_path):
                     coordinates[part] = (x, y, likelihood, color)
                     frame_coordinates[frame_number] = coordinates
 
-        # Open the video
+        #Open the video
         cap = cv2.VideoCapture(video_path)
         cap2 = cv2.VideoCapture(video_path)
 
-        # Check if the video was opened correctly
+        #Check if the video was opened correctly
         if not cap.isOpened():
             print("Error al abrir el video")
             cap.release()
             return
 
-        # Check the number of frames of the video (optional)
+        #Check the number of frames of the video (optional)
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         print(f"Total de frames en el video: {total_frames}")
 
-        # Process each frame
+        #Process each frame
         for i in tqdm(range(total_frames)):  
             ret, frame = cap.read()
             ret2, frame2 = cap2.read()
@@ -108,15 +108,15 @@ def procesar_carpeta(carpeta_path):
                         if likelihood > 0.7:            
                             cv2.imwrite(f'{directorio_destino}/Frame_{i}.png', frame2)
 
-        # Frees the resources and closes the view window
+        #Frees the resources and closes the view window
         cap.release()
         cap2.release()
         cv2.destroyAllWindows()
 
 body_parts = {
-    'Ojo_izquierdo': ((0, 0, 255), 50),  # 'Left eye', Red, size 50
-    'Ojo_derecho': ((0, 255, 0), 60),   # 'Right eye', Green, size 50
+    'Ojo_izquierdo': ((0, 0, 255), 50),  #'Left eye', Red, size 50
+    'Ojo_derecho': ((0, 255, 0), 50),   #'Right eye', Green, size 50
 }
 
 root_path = "/Users/aitanarilla/Desktop/Modelo_caras/Files"
-procesar_video_y_h5(root_path, max_nivel=3)  # Change max_level according to your needs
+procesar_video_y_h5(root_path, max_nivel=3)  #Change max_level if you need it
